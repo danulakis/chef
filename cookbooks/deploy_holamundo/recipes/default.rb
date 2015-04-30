352925d6470c5675543f5aeb7843b03c91dd3179
danulakis <dfernandez@u.uchile.cl>
2015-04-30 12:00:29 -0300

    Update default.rb

--- cookbooks/deploy_holamundo/recipes/default.rb
+++ cookbooks/deploy_holamundo/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2015, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
+# babababa
 remote_file "/var/lib/tomcat7/webapps/holamundo.war" do
   source "http://nexus.uchile.cl:8081/nexus/service/local/artifact/maven/redirect?r=releases&e=war&g=cl.uchile.sti.arq&a=hola-mundo-jsf&v=0.0.4-RELEASE"
   owner 'root'
